9c885a3dedf621c5a8f0d1e7c55cc6b6c8096419
Zhao KK <kikakkz@hotmail.com>
2022-03-18 12:25:38 +0000

    Add invited count

--- pkg/middleware/referral/summary.go
+++ pkg/middleware/referral/summary.go
@@ -98,6 +98,7 @@ func getReferral(ctx context.Context, appID, userID string) (*npool.Referral, er
 		USDAmount:    amount,
 		SubUSDAmount: subAmount,
 		Kol:          len(invitees) > 0,
+		InvitedCount: uint32(len(invitees)),
 	}, nil
 }
 
